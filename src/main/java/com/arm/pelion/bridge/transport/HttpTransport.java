a708e24b5e63024013e95b1a3e724b617a34b09f
Doug Anson <doug.anson@arm.com>
2018-09-07 14:22:24 -0500

    http additional header options added

--- src/main/java/com/arm/pelion/bridge/transport/HttpTransport.java
+++ src/main/java/com/arm/pelion/bridge/transport/HttpTransport.java
@@ -24,6 +24,7 @@ package com.arm.pelion.bridge.transport;
 
 import com.arm.pelion.bridge.core.BaseClass;
 import com.arm.pelion.bridge.core.ErrorLogger;
+import com.arm.pelion.bridge.core.KeyValuePair;
 import com.arm.pelion.bridge.preferences.PreferenceManager;
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -36,6 +37,7 @@ import java.net.URLConnection;
 import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
+import java.util.ArrayList;
 import javax.net.ssl.HttpsURLConnection;
 import javax.net.ssl.SSLContext;
 import org.apache.commons.codec.binary.Base64;
@@ -56,6 +58,7 @@ public class HttpTransport extends BaseClass {
     private String m_pelion_api_hostname = null;
     private PelionHostnameVerifier m_verifier = null;
     private PelionTrustManagerListFactory m_trust_managers = null;
+    private ArrayList<KeyValuePair> m_additional_headers = null;
 
     // constructor
     /**
@@ -65,18 +68,56 @@ public class HttpTransport extends BaseClass {
      */
     public HttpTransport(ErrorLogger error_logger, PreferenceManager preference_manager) {
         super(error_logger, preference_manager);
+        
+        // Pelion API address
         this.m_pelion_api_hostname = preference_manager.valueOf("mds_address");
         if (this.m_pelion_api_hostname == null || this.m_pelion_api_hostname.length() == 0) {
             this.m_pelion_api_hostname = preference_manager.valueOf("api_endpoint_address");
         }
+        
+        // set the hostname verifier
         this.m_verifier = new PelionHostnameVerifier(this.m_pelion_api_hostname);
+        
+        // set the trust managers
         this.m_trust_managers = new PelionTrustManagerListFactory(error_logger, preference_manager);
+        
+        // set the authentication qualifier (typically "bearer")
         String auth_qualifier = this.prefValue("http_auth_qualifier");
         if (auth_qualifier != null && auth_qualifier.length() > 0) {
             this.m_auth_qualifier_default = auth_qualifier;
             this.m_auth_qualifier = this.m_auth_qualifier_default;
         }
         this.errorLogger().info("HTTP: Authorization Qualifier set to: " + this.m_auth_qualifier);
+        
+        // initialize the additional header support
+        this.m_additional_headers = new ArrayList<>();
+    }
+    
+    // add an additional header option
+    public void addHeader(String key,String value) {
+        if (this.hasHeader(key,value) == false) {
+            KeyValuePair kvp = new KeyValuePair(key,value);
+            this.m_additional_headers.add(kvp);
+        }
+    }
+    
+    // clear additional headers
+    public void clearAdditionalHeaders() {
+        this.m_additional_headers.clear();
+    }
+    
+    // check if we already have the requested header 
+    private boolean hasHeader(String key,String value) {
+        boolean has_header = false;
+        KeyValuePair test_kvp = new KeyValuePair(key,value);
+        
+        for(int i=0;i<this.m_additional_headers.size() && !has_header;++i) {
+            if (test_kvp.same(this.m_additional_headers.get(i)) == true) {
+                has_header = true;
+            } 
+        }
+        
+        return has_header;
     }
 
     // set the authorization qualifier
@@ -330,151 +371,148 @@ public class HttpTransport extends BaseClass {
         try {
             URL url = new URL(url_str);
 
-            // Http Connection and verb
-            if (doSSL) {
-                // Install our pelion trust manager and hostname verifier
-                try {
-                    sc = SSLContext.getInstance("TLS");
-                    sc.init(null, this.m_trust_managers.create(), new SecureRandom());
-                    HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
-                    HttpsURLConnection.setDefaultHostnameVerifier(this.m_verifier);
-                    setup = true;
-                }
-                catch (NoSuchAlgorithmException | KeyManagementException e) {
-                    // ERROR - unable to setup/config TLS
-                    this.errorLogger().critical("ERROR: doHTTP() exception during TLS config: " + e.getMessage(),e);
-                }
-                
-                // check if we have TLS setup/configured thus far...
-                if (setup == true) {
-                    // open the SSL connction
-                    connection = (HttpsURLConnection) (url.openConnection());
-                    ((HttpsURLConnection) connection).setRequestMethod(verb);
-                    ((HttpsURLConnection) connection).setSSLSocketFactory(sc.getSocketFactory());
-                    ((HttpsURLConnection) connection).setHostnameVerifier(this.m_verifier); 
+            // Install our pelion trust manager and hostname verifier
+            try {
+                sc = SSLContext.getInstance("TLS");
+                sc.init(null, this.m_trust_managers.create(), new SecureRandom());
+                HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+                HttpsURLConnection.setDefaultHostnameVerifier(this.m_verifier);
+                setup = true;
+            }
+            catch (NoSuchAlgorithmException | KeyManagementException e) {
+                // ERROR - unable to setup/config TLS
+                this.errorLogger().critical("HttpTransport(): doHTTP() exception during TLS config: " + e.getMessage(),e);
+            }
 
-                    // Input configuration
-                    connection.setDoInput(doInput);
-                    if (doOutput && data != null && data.length() > 0) {
-                        connection.setDoOutput(doOutput);
-                    }
-                    else {
-                        connection.setDoOutput(false);
-                    }
+            // check if we have TLS setup/configured thus far...
+            if (setup == true) {
+                // open the SSL connction
+                connection = (HttpsURLConnection) (url.openConnection());
+                ((HttpsURLConnection) connection).setRequestMethod(verb);
+                ((HttpsURLConnection) connection).setSSLSocketFactory(sc.getSocketFactory());
+                ((HttpsURLConnection) connection).setHostnameVerifier(this.m_verifier); 
 
-                    // enable basic auth if requested
-                    if (use_api_token == false && username != null && username.length() > 0 && password != null && password.length() > 0) {
-                        String encoding = Base64.encodeBase64String((username + ":" + password).getBytes());
-                        connection.setRequestProperty("Authorization", this.m_basic_auth_qualifier + " " + encoding);
-                        this.errorLogger().info("HTTP(S): Basic Authorization: " + username + ":" + password + ": " + encoding);
-                    }
-
-                    // enable ApiTokenAuth auth if requested
-                    if (use_api_token == true && api_token != null && api_token.length() > 0) {
-                        // use qualification for the authorization header...
-                        connection.setRequestProperty("Authorization", this.m_auth_qualifier + " " + api_token);
-                        this.errorLogger().info("HTTP(S): ApiTokenAuth Authorization: " + this.m_auth_qualifier + " " + api_token);
-
-                        // Always reset to the established default
-                        this.resetAuthorizationQualifier();
-                    }
-
-                    // ETag support if requested
-                    if (this.m_etag_value != null && this.m_etag_value.length() > 0) {
-                        // set the ETag header value
-                        connection.setRequestProperty("ETag", this.m_etag_value);
-                        //this.errorLogger().info("ETag Value: " + this.m_etag_value);
-
-                        // Always reset to the established default
-                        this.resetETagValue();
-                    }
-
-                    // If-Match support if requested
-                    if (this.m_if_match_header_value != null && this.m_if_match_header_value.length() > 0) {
-                        // set the If-Match header value
-                        connection.setRequestProperty("If-Match", this.m_if_match_header_value);
-                        //this.errorLogger().info("If-Match Value: " + this.m_if_match_header_value);
-
-                        // Always reset to the established default
-                        this.resetIfMatchValue();
-                    }
-
-                    // specify content type if requested
-                    if (content_type != null && content_type.length() > 0) {
-                        connection.setRequestProperty("Content-Type", content_type);
-                        connection.setRequestProperty("Accept", "*/*");
-
-                        // DEBUG
-                        this.errorLogger().info("ContentType: " + content_type);
-                    }
-
-                    // add Connection: keep-alive (does not work...)
-                    //connection.setRequestProperty("Connection", "keep-alive");
-                    // special gorp for HTTP DELETE
-                    if (verb != null && verb.equalsIgnoreCase("delete")) {
-                        connection.setRequestProperty("Access-Control-Allow-Methods", "OPTIONS, DELETE");
-                    }
-
-                    // specify a persistent connection or not
-                    if (persistent == true) {
-                        connection.setRequestProperty("Connection", "keep-alive");
-                    }
-
-                    // DEBUG dump the headers
-                    //if (doSSL) 
-                    //    this.errorLogger().info("HTTP: Headers: " + ((HttpsURLConnection)connection).getRequestProperties()); 
-                    //else
-                    //    this.errorLogger().info("HTTP: Headers: " + ((HttpURLConnection)connection).getRequestProperties()); 
-                    // specify data if requested - assumes it properly escaped if necessary
-                    if (doOutput && data != null && data.length() > 0) {
-                        try (OutputStreamWriter out = new OutputStreamWriter(connection.getOutputStream(),"UTF-8")) {
-                            this.errorLogger().info("HTTP(" + verb + "): DATA: " + data + " CONTENT_TYPE: " + content_type);
-                            out.write(data);
-                            out.flush();
-                            out.close();
-                        }
-                    }
-
-                    // setup the output if requested
-                    if (doInput) {
-                        try {
-                            try (InputStream content = (InputStream) connection.getInputStream(); BufferedReader in = new BufferedReader(new InputStreamReader(content))) {
-                                StringBuilder buf = new StringBuilder();
-                                while ((line = in.readLine()) != null) {
-                                    buf.append(line);
-                                }
-                                result = buf.toString();
-                            }
-                        }
-                        catch (java.io.FileNotFoundException ex) {
-                            this.errorLogger().info("HTTP(" + verb + ") empty response (OK).");
-                            result = "";
-                        }
-                    }
-                    else {
-                        // no result expected
-                        result = "";
-                    }
-
-                    // save off the HTTP response code...
-                    this.saveResponseCode(((HttpsURLConnection) connection).getResponseCode());
-
-                    // DEBUG
-                    //this.errorLogger().info("HTTP(" + verb +") URL: " + url_str + " Data: " + data + " Response code: " + ((HttpsURLConnection)connection).getResponseCode());
+                // Input configuration
+                connection.setDoInput(doInput);
+                if (doOutput && data != null && data.length() > 0) {
+                    connection.setDoOutput(doOutput);
                 }
                 else {
-                    // non-SSL not supported
-                    this.errorLogger().critical("ERROR! doHTTP(): HTTP now unsupported in doHTTP(" + verb + "): URL: " + url_str);
+                    connection.setDoOutput(false);
                 }
+
+                // enable basic auth if requested
+                if (use_api_token == false && username != null && username.length() > 0 && password != null && password.length() > 0) {
+                    String encoding = Base64.encodeBase64String((username + ":" + password).getBytes());
+                    connection.setRequestProperty("Authorization", this.m_basic_auth_qualifier + " " + encoding);
+                    this.errorLogger().info("HttpTransport(): Basic Authorization: " + username + ":" + password + ": " + encoding);
+                }
+
+                // enable ApiTokenAuth auth if requested
+                if (use_api_token == true && api_token != null && api_token.length() > 0) {
+                    // use qualification for the authorization header...
+                    connection.setRequestProperty("Authorization", this.m_auth_qualifier + " " + api_token);
+                    this.errorLogger().info("HttpTransport(): ApiTokenAuth Authorization: " + this.m_auth_qualifier + " " + api_token);
+
+                    // Always reset to the established default
+                    this.resetAuthorizationQualifier();
+                }
+
+                // ETag support if requested
+                if (this.m_etag_value != null && this.m_etag_value.length() > 0) {
+                    // set the ETag header value
+                    connection.setRequestProperty("ETag", this.m_etag_value);
+                    //this.errorLogger().info("ETag Value: " + this.m_etag_value);
+
+                    // Always reset to the established default
+                    this.resetETagValue();
+                }
+
+                // If-Match support if requested
+                if (this.m_if_match_header_value != null && this.m_if_match_header_value.length() > 0) {
+                    // set the If-Match header value
+                    connection.setRequestProperty("If-Match", this.m_if_match_header_value);
+                    //this.errorLogger().info("If-Match Value: " + this.m_if_match_header_value);
+
+                    // Always reset to the established default
+                    this.resetIfMatchValue();
+                }
+
+                // specify content type if requested
+                if (content_type != null && content_type.length() > 0) {
+                    connection.setRequestProperty("Content-Type", content_type);
+                    connection.setRequestProperty("Accept", "*/*");
+
+                    // DEBUG
+                    this.errorLogger().info("ContentType: " + content_type);
+                }
+
+                // special headers for HTTPS DELETE
+                if (verb != null && verb.equalsIgnoreCase("delete")) {
+                    connection.setRequestProperty("Access-Control-Allow-Methods", "OPTIONS, DELETE");
+                }
+
+                // specify a persistent connection or not
+                if (persistent == true) {
+                    connection.setRequestProperty("Connection", "keep-alive");
+                }
+
+                // add any additional headers
+                if (this.m_additional_headers != null && this.m_additional_headers.size() > 0) {
+                    for(int i=0;i<this.m_additional_headers.size();++i) {
+                        KeyValuePair kvp = this.m_additional_headers.get(i);
+                        connection.setRequestProperty(kvp.key(), kvp.value());
+                    }
+                }
+
+                // DEBUG -  dump the headers
+                this.errorLogger().info("HttpTransport(" + verb + "): Headers: " + ((HttpsURLConnection)connection).getRequestProperties()); 
+
+                // specify data if requested - assumes it properly escaped if necessary
+                if (doOutput && data != null && data.length() > 0) {
+                    try (OutputStreamWriter out = new OutputStreamWriter(connection.getOutputStream(),"UTF-8")) {
+                        this.errorLogger().info("HttpTransport(" + verb + "): DATA: " + data + " CONTENT_TYPE: " + content_type);
+                        out.write(data);
+                        out.flush();
+                        out.close();
+                    }
+                }
+
+                // setup the output if requested
+                if (doInput) {
+                    try {
+                        try (InputStream content = (InputStream) connection.getInputStream(); BufferedReader in = new BufferedReader(new InputStreamReader(content))) {
+                            StringBuilder buf = new StringBuilder();
+                            while ((line = in.readLine()) != null) {
+                                buf.append(line);
+                            }
+                            result = buf.toString();
+                        }
+                    }
+                    catch (java.io.FileNotFoundException ex) {
+                        this.errorLogger().info("HttpTransport(" + verb + ") empty response (OK).");
+                        result = "";
+                    }
+                }
+                else {
+                    // no result expected
+                    result = "";
+                }
+
+                // save off the HTTP response code...
+                this.saveResponseCode(((HttpsURLConnection) connection).getResponseCode());
+
+                // DEBUG
+                //this.errorLogger().info("HTTP(" + verb +") URL: " + url_str + " Data: " + data + " Response code: " + ((HttpsURLConnection)connection).getResponseCode());
             }
             else {
-                // unable to setup SSL 
-                this.errorLogger().critical("ERROR: doHTTP(): Unable to configure/setup TLS! Aborting...");
+                // non-SSL not supported
+                this.errorLogger().critical("ERROR! HttpTransport(): HTTP now unsupported in doHTTP(" + verb + "): URL: " + url_str);
             }
         }    
         catch (IOException ex) {
             // exception note (DEBUG)
-            this.errorLogger().info("Exception in doHTTP(" + verb + "): URL: " + url_str +  " ERROR: " + ex.getMessage());
+            this.errorLogger().info("HttpTransport(): Exception in doHTTP(" + verb + "): URL: " + url_str +  " ERROR: " + ex.getMessage());
             result = null;
 
             try {
@@ -484,11 +522,11 @@ public class HttpTransport extends BaseClass {
                     this.saveResponseCode(((HttpsURLConnection) connection).getResponseCode());
                 }
                 else {
-                    this.errorLogger().warning("ERROR in doHTTP(" + verb + "): Connection is NULL");
+                    this.errorLogger().warning("HttpTransport(): ERROR in doHTTP(" + verb + "): Connection is NULL");
                 }
             }
             catch (IOException ex2) {
-                this.errorLogger().warning("Exception in doHTTP(" + verb + "): Unable to save last response code: " + ex2.getMessage());
+                this.errorLogger().warning("HttpTransport(): Exception in doHTTP(" + verb + "): Unable to save last response code: " + ex2.getMessage());
             }
         }
 
